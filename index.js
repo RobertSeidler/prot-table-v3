77cdb7ea9679aecf00322af78341e182fd947acb
Robert Seidler <robert.seidler1@googlemail.com>
2022-01-02 14:43:26 +0100

    Added markInput; updated wc-grid-table to version 3.0.0;

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let wcGridTable = require("wc-grid-table/src/wc-grid-table.js");
+let { MarkInput, fetchSelectCheckedValues } = require("./MarkInput");
 
 require('./style.css');
 // wcGridTable.defineCustomElement()
@@ -71,13 +72,94 @@ class ProtTable extends wcGridTable.TableComponent {
         if (jsonUrl) {
             fetch(jsonUrl)
                 .then(data => data.json())
-                .then(data => this.setData(data))
+                .then(data => {
+                    this.setupMarkInputs(data)
+                        .then(markData => this.setData(markData));
+                });
         }
+    }
+
+    /**
+     * Sets up the marker column, but only when all of the required attributes exist.
+     * @param {object} data - table data
+     * @returns {object} - table data
+     */
+    async setupMarkInputs(data) {
+        const requiredAttributes = {
+            identifierField: this.getAttribute('marker_identifierfield'),
+            databaseTable: this.getAttribute('marker_databasetable'),
+        };
+
+        const optionalAttributes = {
+            database: this.getAttribute('marker_database'),
+            databaseuser: this.getAttribute('marker_databaseuser'),
+            markerText: this.getAttribute('marker_markertext'),
+        };
+
+        if (Reflect.ownKeys(requiredAttributes).map((key) => requiredAttributes[key]).every((value) => (value === undefined ? false : true))) {
+            // console.log(data);
+            return this.generateMarkInputData(data, requiredAttributes, optionalAttributes);
+        } else {
+            return data;
+        }
+    }
+
+    /**
+     * Creates a MarkInput element.
+     * @param {string} identifierField
+     * @param {string} identifierValue
+     * @param {string} tablename
+     * @param {string} database
+     * @param {string} dbuser
+     * @param {string} marker
+     * @param {boolean} checked
+     * @returns {string} - MarkInput outer html
+     */
+    createMarkInput(identifierField, identifierValue, tablename, database, dbuser, marker, checked) {
+        let markInput = document.createElement('mark-input');
+        markInput.setAttribute('identifierfield', identifierField);
+        markInput.setAttribute('identifiervalue', identifierValue);
+        if (tablename) markInput.setAttribute('databasetable', tablename);
+        if (database) markInput.setAttribute('database', database);
+        if (dbuser) markInput.setAttribute('databaseuser', dbuser);
+        if (marker) markInput.setAttribute('markertext', marker);
+        if (checked) markInput.toggleAttribute('checked', checked);
+        return markInput.outerHTML;
+    }
+
+    /**
+     * Generates the data for the table, which includes a row with MarkerInputs.
+     * @param {object} data - table data
+     * @param {{identifierField: string, databaseTable: string}} reqAttr - required MarkInput attributes
+     * @param {{database?: string, databaseuser?: string, markerText?: string}} optAttr - optional MarkInput attributes
+     * @returns {object} - table data
+     */
+    async generateMarkInputData(data, reqAttr, optAttr) {
+        let { identifierField, databaseTable } = reqAttr;
+        let { database, databaseuser, markerText } = optAttr;
+
+        database = database ? database : "MarkerDB";
+        databaseuser = databaseuser ? databaseuser : "wiki";
+        // databaseTable = databaseTable ? databaseTable : "DefaultTable";
+        // markerText = markerText ? markerText : "jjj|nnn";
+
+        return fetchSelectCheckedValues(database, databaseuser, databaseTable, identifierField)
+            .then((checkedData) => {
+                return data.map((entry) => {
+                    let checked = checkedData.map((value) => value.identifierValue).includes(entry[identifierField].toString());
+                    return {
+                        'marker': this.createMarkInput(identifierField, entry[identifierField].toString(), databaseTable, database, databaseuser, markerText, checked),
+                        ...entry,
+                    };
+                });
+            });
     }
 }
 
+customElements.define('mark-input', MarkInput);
 customElements.define('prot-table-v3', ProtTable);
 
-
-
-// console.log()
+module.exports = {
+    ProtTable: ProtTable,
+    TableComponent: wcGridTable.TableComponent,
+};
